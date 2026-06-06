2ebab376e06639e81690f8d5e69a2638d31387d8
Damilare Osunrinde <89488280+Damilare-prog@users.noreply.github.com>
2026-06-06 01:18:16 +0100

    Fix syntax for dangerouslySetInnerHTML

--- app/page.tsx
+++ app/page.tsx
@@ -112,7 +112,7 @@ export default function Home() {
 
   return (
     <div
-      dangerouslySetInnerHTML={ __html: getHTML() }
+      dangerouslySetInnerHTML={{ __html: getHTML() }}
       suppressHydrationWarning
     />
   );
